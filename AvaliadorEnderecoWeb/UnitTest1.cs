91cb8b3738541348817a2046662150ee829b41c4
Abner Terribili <abner.terribili@gmail.com>
2014-02-10 11:38:37 -0200

    Criacao dos Testes de Unidade e Classe de Processamento

---
+++ AvaliadorEnderecoWeb/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AvaliadorEnderecosWeb;
+
+namespace AvaliadorEnderecoWeb
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void DeveRetornarBuscapeCasoSiteSejaBuscape()
+        {
+            ProcessadorDominio dominio = new ProcessadorDominio("buscape");
+
+            Assert.AreEqual("http://www.buscape.com.br", dominio.ProcessarDominio());
+        }
+
+        [TestMethod]
+        public void DeveRetornarBondfaroCasoSiteSejaBondfaro()
+        {
+            ProcessadorDominio dominio = new ProcessadorDominio("bondfaro");
+
+            Assert.AreEqual("http://www.bondfaro.com.br", dominio.ProcessarDominio());
+        }
+
+        [TestMethod]
+        public void DeveRetornarZmoveisCasoSiteSejaZmoveis()
+        {
+            ProcessadorDominio dominio = new ProcessadorDominio("zmoveis");
+
+            Assert.AreEqual("http://www.zmoveis.com.cl", dominio.ProcessarDominio());
+        }
+    }
+}
---
+++ AvaliadorEnderecosWeb/ProcessadorDominio.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvaliadorEnderecosWeb
+{
+    public class ProcessadorDominio
+    {
+        private readonly String nome;
+
+        public ProcessadorDominio(String nome)
+        {
+            this.nome = nome;
+        }
+
+        public String ProcessarDominio()
+        {
+            if (nome == "buscape")
+                return "http://www.buscape.com.br";
+            if (nome == "bondfaro")
+                return "http://www.bondfaro.com.br";
+            if (nome == "zmoveis")
+                return "http://www.zmoveis.com.cl";
+            else
+                return "";
+        }
+    }
+}
